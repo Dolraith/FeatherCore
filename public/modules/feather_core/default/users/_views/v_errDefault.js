8ea6edcae1f1b565e37f602b30d5e4583c7e0f53
Dolraith <Dolraith@Gmail.com>
2022-12-20 10:38:19 -0500

    removing vue where there waas no vue

--- public/modules/feather_core/default/users/_views/v_errDefault.js
+++ public/modules/feather_core/default/users/_views/v_errDefault.js
@@ -3,7 +3,6 @@ class v_login extends View{
     constructor(){        
         super();
         this.addDependency('bootstrap');
-        this.addDependency('vue');
         this.addDependency(['modules/feather_core/default/users/_js/j_login.js'],'js')
         this.setTemplate('modules/feather_core/default/users/_templates/t_errDefault.html');
     }    
